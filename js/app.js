006527daaf3fbfa259e1accf9ac44e1fc1eb9950
ZooM982 <134058804+ZooM982@users.noreply.github.com>
2023-07-25 21:58:18 +0000

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// une ligne de commentaire
+
+/* un commentaire 
+sur plusieurs
+lignes*/ 
+
+//ouvrir une boite de dialogue
+
+//alert('Bonjour je suis le fichier javascript');
+
+//Texte en console
+console.log('Texte en console');
